11c40bcda9dbb3a93f45137be751250146f7b6ed
NanaNiki <nicol.wesolowska@gmail.com>
2023-07-11 22:49:38 +0200

    "svgs"

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>The Zen Art of Forgivness</h1>
-       
+        <img src='/images/roottree.svg' alt='sakura tree' className='sakura'/>
+        <div className='flowers'>
+        <img src='/images/flowers.svg' alt='sakura flower' className='flower flower1'/> 
+        <img src='/images/flowers.svg' alt='sakura flower' className='flower flower2'/> 
+        <img src='/images/flowers.svg' alt='sakura flower' className='flower flower3'/> 
+        <img src='/images/flowers.svg' alt='sakura flower' className='flower flower4'/> 
+        <img src='/images/flowers.svg' alt='sakura flower' className='flower flower5'/> 
+        <img src='/images/flowers.svg' alt='sakura flower' className='flower flower6'/> 
+        <img src='/images/flowers.svg' alt='sakura flower' className='flower flower7'/> 
+        </div>
       </header>
+
     </div>
   );
 }
